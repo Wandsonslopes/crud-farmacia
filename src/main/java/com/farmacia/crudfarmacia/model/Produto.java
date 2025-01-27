f3a2906b03d351a2af7ef07a3d2bd40b9e617b88
Wandsonslopes <wandson_lopes@hotmail.com>
2025-01-27 16:12:09 -0300

    Incluindo CRUD do Produto

---
+++ src/main/java/com/farmacia/crudfarmacia/model/Produto.java
@@ -0,0 +1,75 @@
+package com.farmacia.crudfarmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Nome é Obrigatório!")
+    private String nome;
+	
+	@NotBlank(message = "O atributo Descrição é Obrigatório!")
+    private String descricao;
+	
+	@NotNull(message = "O valor do preço não pode ser nulo!")
+	private Double preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("categoria")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
